9b0d19aa610566cab08399387590453cca8cb350
guilmeister <ebg.edwardbguillermo@yahoo.com>
2020-02-07 23:48:42 +0000

    partial main, call functions

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Entry point of Codebreaker"""
+
+print('Number of Players? ', end = '')
+number_players = input('')
+
+print('How many rounds to play? ', end = '')
+number_rounds = input('')
+
+print('Players: {}, Rounds: {}'.format(number_players,
+                                       number_rounds))
+
+#Call functions
