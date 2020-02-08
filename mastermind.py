06d87f6d769605ab77557696f6a615151318d6ca
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-02-07 17:30:39 -0800

    Added command-line option parser to mastermind.py

--- mastermind.py
+++ mastermind.py
@@ -1,14 +1,28 @@
 #!/usr/bin/python3
-"""Entry point of Codebreaker"""
-from mastermind.player import interactive_player
+"""Play Mastermind"""
+import argparse
+import os
+import sys
+import mastermind
 
-print('Number of Players? ', end = '')
-number_players = input('')
 
-print('How many rounds to play? ', end = '')
-number_rounds = input('')
+def require(condition):
+    """Get a subclass of Action that requires the given condition to be met"""
+    class Require(argparse.Action):
+        def __call__(self, parser, namespace, values, option_string=None):
+            if condition(values) is True:
+                setattr(namespace, self.dest, values)
+            else:
+                parser.error(parser.format_help())
+    return Require
 
-print('Players: {}, Rounds: {}'.format(number_players,
-                                       number_rounds))
 
-#Call functions
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog=os.path.basename(sys.argv[0]))
+    parser.add_argument('--rounds', type=int,
+                        action=require(lambda i: i % 2 == 0),
+                        help='number of rounds (must be an even number)')
+    parser.add_argument('--noninteractive',
+                        action='store_true',
+                        help='machine vs. machine')
+    args = parser.parse_args()
